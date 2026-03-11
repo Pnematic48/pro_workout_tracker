fbdc650f10146cd45371c97398f8c0c65bde4eb6
Pnematic48 <mo.sarps.48@gmail.com>
2026-03-11 15:09:56 +0000

    adding changes

---
+++ workout.py
@@ -0,0 +1,24 @@
+
+exercises={
+    "exercise1":{
+        "name" : "Squats",
+        "sets"  : 4,
+        "reps"  : 10,
+        "weight": 50
+    },
+    "exercise2":{
+        "name" : "Shoulder press",
+        "sets"  : 4,
+        "reps"  : 10,
+        "weight": 20
+    },
+    "exercise3":{
+        "name" : "deadlift",
+        "sets"  : 4,
+        "reps"  : 10,
+        "weight": 50
+    }
+}
+
+for key in exercises:
+    
