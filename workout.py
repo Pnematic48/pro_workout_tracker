117f6b8a253883ce13241f38a8cfaeb236dd5f62
JacklinPrempeh <Jackiep6132@gmail.com>
2026-03-15 11:33:10 +0000

    implementing save session,load  session,and log session

--- workout.py
+++ workout.py
@@ -1,24 +1,54 @@
 
-exercises={
-    "exercise1":{
-        "name" : "Squats",
-        "sets"  : 4,
-        "reps"  : 10,
-        "weight": 50
-    },
-    "exercise2":{
-        "name" : "Shoulder press",
-        "sets"  : 4,
-        "reps"  : 10,
-        "weight": 20
-    },
-    "exercise3":{
-        "name" : "deadlift",
-        "sets"  : 4,
-        "reps"  : 10,
-        "weight": 50
-    }
-}
 
-for key in exercises:
-    
+import datetime
+def save_sessions(sessions):
+    file = open("workouts.txt", "w")
+    file.write(str(sessions))
+    file.close()
+
+def load_sessions():
+    try:
+        file = open("workouts.txt", "r")
+        data = file.read()
+        file.close()
+        return eval(data)
+    except:
+        return []
+
+
+
+def log_session(sessions):
+
+    exercises = []
+
+    while True:
+
+        name = input("Exercise name (or done): ")
+
+        if name == "done":
+            break
+
+        sets = int(input("Sets: "))
+        reps = int(input("Reps: "))
+        weight = float(input("Weight (0 for bodyweight): "))
+
+        exercise = {
+            "name": name,
+            "sets": sets,
+            "reps": reps,
+            "weight": weight
+        }
+
+        exercises.append(exercise)
+
+    session = {
+        "date": str(datetime.date.today()),
+        "exercises": exercises
+    }
+
+    sessions.append(session)
+
+    return session
+
+
+
