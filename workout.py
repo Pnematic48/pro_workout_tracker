cc845555c13800fe5fe3c1013a5f64e5cae11598
JacklinPrempeh <Jackiep6132@gmail.com>
2026-03-15 12:22:36 +0000

    adding volume calculation and session viewing

--- workout.py
+++ workout.py
@@ -3,53 +3,75 @@
 # Tech4Girls Backend Cohort 4
 
 
-import datetime
-def save_sessions(sessions):
-    file = open("workouts.txt", "w")
-    file.write(str(sessions))
-    file.close()
+import datetime # automatically capture today's date.
+def save_sessions(sessions):   
+    file = open("workouts.txt", "w") # opens file called works.txt inw write mode
+    file.write(str(sessions)) # changes session list into string and saves it into file
+    file.close() # closes file after
 
 def load_sessions():
     try:
-        file = open("workouts.txt", "r")
-        data = file.read()
+        file = open("workouts.txt", "r")  #opens file called works.txt in read mode
+        data = file.read()  #reads content of  file
         file.close()
-        return eval(data)
+        return eval(data) # changes text back to python list
     except:
-        return []
+        return [] # if file does not exist, return an empty list
 
 
 
 def log_session(sessions):
 
-    exercises = []
+    exercises = [] # create an empty list for storing exercises in woerkout session
 
-    while True:
+    while True: # for multiple exercise addition
 
-        name = input("Exercise name (or done): ")
+        name = input("Exercise name (or end): ") # request user for name  or end to stop asking
 
-        if name == "done":
-            break
+        if name == "end":  #stops adding exercise if user types end
+            break  
 
-        sets = int(input("Sets: "))
-        reps = int(input("Reps: "))
-        weight = float(input("Weight (0 for bodyweight): "))
+        sets = int(input("Sets: ")) # askes user for number of sets
+        reps = int(input("Reps: ")) # askes user for number of reps
+        weight = float(input("Weight (0 for bodyweight): ")) # askes user for weight used
 
-        exercise = {
+        exercise = { # create a dictionary to store eexercise information
             "name": name,
             "sets": sets,
             "reps": reps,
             "weight": weight
         }
 
-        exercises.append(exercise)
+        exercises.append(exercise) # adds exercise dictionary to exercise list
 
-    session = {
-        "date": str(datetime.date.today()),
-        "exercises": exercises
+
+    session = { # add session dictionary
+        "date": str(datetime.date.today()), # automatically stores date
+        "exercises": exercises # stores all exercises in session
     }
 
-    sessions.append(session)
+    sessions.append(session) # adds session to list of sessions
 
-    return session
+    return session # returns a new session
+
+def calculate_volume(exercise): # function for volume calculation
+
+    weight = max(exercise["weight"], 1) # fi weight =0 for bodyweight , use 1 instead
+
+    return exercise["sets"] * exercise["reps"] * weight # calculates exercise volume
+
+
+def view_all_sessions(sessions): #function for viewing sessions
+
+    for session in sessions: # loops through all saved workout sessions
+
+        total_volume = 0 #resets total volume for each session
+
+        for exercise in session["exercises"]: # loop through exercises in session
+            volume = calculate_volume(exercise) 
+            total_volume += calculate_volume(exercise) # adds exercise volume to total
+
+        print("Date:", session["date"]) # prints session date
+        print("Total Volume:", total_volume) # prints total volume 
+   
 
